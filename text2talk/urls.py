07f28e75fb0e47af60e43711c61adef0f3b7811c
Yujinndev <xyujinwho@gmail.com>
2024-03-10 23:05:30 +0800

    feat: add and implement the text-to-speech using gtts

--- text2talk/urls.py
+++ text2talk/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path 
 from text2talk import views 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-	path("", views.index),
-	path("ocr", views.ocr),
+	path("", views.index, name="index"),
+	path("textOcr/", views.image_to_text, name="textOcr"),
+	path("talkOcr/", views.text_to_speech, name="talkOcr")
+	# path("live/", views.video_feed, name="live"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static('/images/', document_root=settings.STATICFILES_DIRS[0])
--- text2talk/views.py
+++ text2talk/views.py
@@ -1,19 +1,38 @@
-from django.shortcuts import render
-from django.conf import settings
-from PIL import Image
-import pytesseract
 import os
+import pytesseract
+from text2talk import forms, models
+from django.conf import settings
+from django.shortcuts import render, redirect
+from PIL import Image
+from gtts import gTTS
+
+uploaded_image_name = "text2talk\static\images\placeholder-image.png"
 
 def index(request): 
 	return render(request, "index.html", {})
 
-def ocr(request):
+def perform_ocr_helper(image_name):
     static_dir = settings.STATICFILES_DIRS[0]
-    image_path = os.path.join(static_dir, 'images\hardwork.jpg')
-    
+    image_path = os.path.join(static_dir, image_name)
+
     image = Image.open(image_path)
-
     # conversion using the pytesseract lib
-    text_result = pytesseract.image_to_string(image, lang='eng')
+    ocr_result = pytesseract.image_to_string(image, lang='eng')
+    return ocr_result
 
-    return render(request, 'ocr.html', {'text_result': text_result})
+def image_to_text(request):
+    result = perform_ocr_helper(uploaded_image_name)
+
+    return render(request, 'textOcr.html', {'text_result': result, 'image': uploaded_image_name})
+
+def text_to_speech(request):
+    static_dir = settings.STATICFILES_DIRS[2]
+    result = perform_ocr_helper(uploaded_image_name)
+
+    # conversion from the text to audio
+    tts = gTTS(text=result, lang='en')
+    os.makedirs(static_dir, exist_ok=True)
+    output_path = os.path.join(static_dir, 'tts.mp3')
+    tts.save(output_path)
+
+    return render(request, 'talkOcr.html', {'image': uploaded_image_name})
